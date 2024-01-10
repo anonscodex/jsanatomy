92f0cfd6eb489b90cc6635d6ee2386a41c53c06f
normalstuff2001@gmail.com <normalstuff2001@gmail.com>
2024-01-10 23:55:09 +0100

    remove the odd code

--- math.js
+++ math.js
@@ -16,10 +16,6 @@ solve_equation = (a, b, c)=> {
 }
 
 
-solve_equation2 = (a,b,c)=> {
-  return [x1(a, b, c), x2(a, b, c)];
-}
-
 console.log(solve_equation(1, 5, 6));
-console.log(solve_equation2(5, 1, 6));
+
 ;
